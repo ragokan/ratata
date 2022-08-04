d0d21bf2b1c6f2c85cc4302f40e93aa2ade3d21f
ragokan <okanyldrmop@gmail.com>
2022-08-04 16:20:27 +0300

    cl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Get } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { AppService } from "./app.service";
 
+class ReturnType {
+  message: string;
+}
+
 @ApiTags("App")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): ReturnType {
+    return this.appService.getHello() as ReturnType;
   }
 }
