d04c8ff7f3cf24b2ff2df59104c56303c8ee5d56
ragokan <okanyldrmop@gmail.com>
2022-08-27 22:08:24 +0300

    entity upgrade

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,7 +1,10 @@
 import { MinLength } from "class-validator";
 import { BaseEntity } from "src/common/database/entity/base.entity";
+import { UserEntity } from "src/user/entities/user.entity";
 
 export class PostEntity extends BaseEntity {
   @MinLength(5, { message: "Min length cannot be shorter than 5 letters." })
   title: string;
+
+  userId: number;
 }
